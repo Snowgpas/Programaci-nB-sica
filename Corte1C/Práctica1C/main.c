db6cafb690f094d31552900b0710fdfa819b5bf3
Snowgpas <gabor.2007uwu@gmail.com>
2025-09-12 13:34:20 -0500

    Add files via upload

---
+++ Corte1C/Práctica1C/main.c
@@ -0,0 +1,15 @@
+#include <iostream>
+
+/*
+ run this program using the console pauser or add your own getch, system("pause") or input loop 
+Hola, soy insano
+
+*/
+
+//jijijija
+
+int main(int argc, char** argv) {
+	
+	printf("Hola mundo");
+	return 0;
+}
