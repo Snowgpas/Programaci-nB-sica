0f8663de7ae4cd3b057f85ff49f630fbfd750e13
Snowgpas <gabor.2007uwu@gmail.com>
2025-09-27 21:35:27 -0500

    Add files via upload

---
+++ Corte1 C/Práctica 5/main suma resta y multiplicación.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int a = 7;
+	int b = 4;
+	int suma = a + b;
+	
+	printf("El resultado de sumar %d y %d es: %d\n", a, b, suma);
+
+  int c = 6;
+  int d = 3;
+  int resta = c - d;
+
+printf("El resultado de restar %d y %d es: %d\n", c, d, resta);
+
+  int e = 5;
+  int f = 8;
+  int multiplicacion = e * f;
+  
+  printf("El resultado de multiplicar %d y %d es: %d\n", e, f, multiplicacion);
+
+
+	return 0;
+}
+
