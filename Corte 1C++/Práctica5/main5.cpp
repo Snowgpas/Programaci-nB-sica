250a0623ea6ba4f4e74e91f62a9a835dd972c91d
Snowgpas <gabor.2007uwu@gmail.com>
2025-09-28 19:42:30 -0500

    Add files via upload

---
+++ Corte 1C++/Práctica5/main5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+	using namespace std;
+	
+	int a, b, c, d, e, f, g, h, i;
+	
+	int main() {
+		
+		a = 5;
+		b = 7;
+		c = a + b;
+		
+		cout << " El resultado de sumar " << a << " y " << b << " es: " << c << '\n';
+		
+		d = 5;
+		e = 7;
+	    f = d - e;
+	    
+	    cout << " El resultado de restar " << d << " y " << e << " es: " << f << '\n';
+	    
+	    g = 5;
+	    h = 7;
+	    i = g * h;
+	    
+	    cout << " El resultado de multiplicar " << g << " y " << h << " es: " << i << '\n';
+ 	}
+
+
+
